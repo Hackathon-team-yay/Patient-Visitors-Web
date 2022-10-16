abd5fc8e3adbc7e93290b94adb5186216380cd90
Jake <jakefidler92@gmail.com>
2022-10-16 03:54:57 -0500

    Decent chunk of basic UI is done now. Need to start working on connecting with database and javascript in general now.

---
+++ js stuff/createAccount.js
@@ -0,0 +1,31 @@
+//This is... stuff. I don't know if it's right or not.
+
+function createAccount() {
+    var password = document.getElementById("password").value;
+    var email = document.getElementById("email").value;
+    var firstName = document.getElementById("firstname").value;
+    var lastName = document.getElementById("lastname").value;
+    var address = document.getElementById("address").value;
+    var city = document.getElementById("city").value;
+    var state = document.getElementById("state").value;
+    var zip = document.getElementById("zip").value;
+    var phone = document.getElementById("phone").value;
+    var dataString = '&password=' + password + '&email=' + email + '&firstname=' + firstName + '&lastname=' + lastName + '&address=' + address + '&phone=' + phone;
+    if ( password == '' || email == '' || firstName == '' || lastName == '' || address == '' || phone == '') {
+        alert("Please Fill All Fields");
+    }
+    else {
+        $.post('createAccount.php' {
+            firstName: firstName,
+            lastName: lastName,
+                email: email,
+                password: password,
+                phone: phone,
+                address: address,
+                city: city,
+                state: state,
+                zip: zip
+        });
+    }
+    return false;
+}
